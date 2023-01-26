d163b0a4d2978fb7663ac098558a1071bcc2fff7
DireElf <direelf@Main>
2023-01-26 20:19:04 +0700

    models added

--- src/main/java/it/leader/sightbook/model/City.java
+++ src/main/java/it/leader/sightbook/model/City.java
@@ -1,4 +1,44 @@
 package it.leader.sightbook.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Table(name = "cities")
 public class City {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "city_name", unique = true)
+    private String name;
+
+    @Column(name = "population")
+    private Integer population;
+
+    @Column(name = "has_metro")
+    private Boolean hasMetro;
+
+    @Column(name = "country")
+    private String country;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Sight> sights;
 }
--- src/main/java/it/leader/sightbook/model/Sight.java
+++ src/main/java/it/leader/sightbook/model/Sight.java
@@ -1,4 +1,53 @@
 package it.leader.sightbook.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Table(name = "sights")
 public class Sight {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "sight_name", unique = true)
+    private String name;
+
+    @Column(name = "creation_date")
+    private Date creation;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "sight_type")
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false, foreignKey = @ForeignKey(name = "FK_SIGHTS_CITY_ID_COL"))
+    private City city;
+
+    private enum Type {
+        BUILDING, STRUCTURE, MUSEUM, MONUMENT, RESERVE;
+    }
 }
