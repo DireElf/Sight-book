0fc87ee10f28bebb3d9dbfb55dccc2d6b750c628
DireElf <direelf@Main>
2023-01-26 18:21:36 +0700

    created

---
+++ src/test/java/it/leader/sightbook/SightBookApplicationTests.java
@@ -0,0 +1,13 @@
+package it.leader.sightbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SightBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
